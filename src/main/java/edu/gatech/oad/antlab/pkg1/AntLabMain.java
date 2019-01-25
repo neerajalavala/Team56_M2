736b696291a3c96b69ae980e9013de689c27cca3
Pratik Nallamotu <pratiknallamotu@gmail.com>
2019-01-25 16:05:16 -0500

    added name and gtid

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("flascari3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Pratik Nallamotu");
+                  toPrint += p5.toString("pnallamotu6");
 
         System.out.println(toPrint);
 
