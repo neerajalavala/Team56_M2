6d1100517fcf814989e109f0fa956669572212ec
aidan <aidan.chaplin@gmail.com>
2019-01-24 21:17:12 -0500

    Trying to fix notepad's weird indentation

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -5,27 +5,22 @@ import java.util.*;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
  */
 public class Person2 {
     /** Holds the persons real name */
-
-    public static void main(String[] args) {
-        System.out.println(calc("Aidan"));
-    }
-
     private String name;
-	 	/**
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	public Person2(String pname) {
+	  name = pname;
+	}
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -47,7 +42,7 @@ public class Person2 {
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
